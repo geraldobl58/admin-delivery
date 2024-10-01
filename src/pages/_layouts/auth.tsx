727d4023e081cd399badcbaefdb95774e835f016
Geraldo Luiz <geraldobl58@gmail.com>
2024-10-01 14:17:06 -0300

    feat: Creating login page

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,9 +1,41 @@
+import { Pizza } from "lucide-react";
 import { Outlet } from "react-router-dom";
 
 export function AuthLayout() {
   return (
-    <div>
-      <div>
+    <div className="min-h-screen grid grid-cols-2">
+      <div
+        className="
+          h-full
+          border-r
+          border-foreground/5
+          bg-muted
+          p-10
+          text-muted-foreground
+          flex
+          flex-col
+          justify-between
+        "
+      >
+        <div
+          className="
+            flex
+            items-center
+            gap-3
+            text-lg
+            font-medium
+            text-foreground
+          "
+        >
+          <Pizza className="size-5" />
+          <span className="font-bold">Delivery</span>
+        </div>
+        <footer className="text-sm">
+          Painel do parceiro &copy; admin delivery -{" "}
+          {new Date().getUTCFullYear()}
+        </footer>
+      </div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
