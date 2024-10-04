da270a6c672f0c877857ea9a0c9f2183cb4db50b
Geraldo Luiz <geraldobl58@gmail.com>
2024-10-04 16:24:15 -0300

    feat: Creating order list

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -8,8 +8,15 @@ import {
 import { OrderTableRow } from "./order-table-row";
 import { OrderTableFilters } from "./order-table-filters";
 import { Pagination } from "@/components/pagination";
+import { useQuery } from "@tanstack/react-query";
+import { getOrders } from "@/api/get-orders";
 
 export function Orders() {
+  const { data: result } = useQuery({
+    queryKey: ["orders"],
+    queryFn: getOrders,
+  });
+
   return (
     <>
       <div className="flex flex-col gap-4">
@@ -32,7 +39,10 @@ export function Orders() {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                <OrderTableRow />
+                {result &&
+                  result.orders.map((order) => {
+                    return <OrderTableRow key={order.orderId} order={order} />;
+                  })}
               </TableBody>
             </Table>
           </div>
